0ee0aa5eb81fe0465845a62d86f541a6fcc2c8ed
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2024-05-10 17:33:06 -0300

    refactor: adicionar nomes de variáveis que estão de acordo.

--- resposta-arvore-binaria/main.py
+++ resposta-arvore-binaria/main.py
@@ -2,25 +2,25 @@ import random
 import BinaryTree as bt
 
 if __name__ == '__main__':
-    tamanho = int(input("Informe o tamanho da lista: "))
-    random.seed(tamanho)
+    size = int(input("Informe o tamanho da lista: "))
+    random.seed(size)
     # Adiciona o primeiro valor à árvore binária ou seja o primeiro nó
     l = bt.BinaryTree(random.randint(0, 2**17))
     # contador de elementos que começa em 1 pois já adicionamos o primeiro
     count = 1
 
-    # Adiciona elementos à árvore binária com base no tamanho
-    while count < tamanho:
+    # Adiciona elementos à árvore binária com base no size
+    while count < size:
         num = random.randint(0, 2**17)
         if not l.contains(num):  # Verifica se o número já está na árvore
             l.put(num)
             count += 1
 
-    alvo = random.randint(0, 2**17)
-    while alvo % 2 == 0:
-        alvo = random.randint(0, 2**17)
+    target = random.randint(0, 2**17)
+    while target % 2 == 0:
+        target = random.randint(0, 2**17)
 
-    # Encontrar o número de pares cujo complemento soma para o valor alvo
-    complement_count = l.find_complement(alvo)
+    # Encontrar o número de pares cujo complemento soma para o valor target
+    complement_count = l.find_complement(target)
     print(f"Número de pares cujo complemento soma para {
-          alvo}: {int(complement_count/2)}")
+          target}: {int(complement_count/2)}")
